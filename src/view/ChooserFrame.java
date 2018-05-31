607fbb6f261c0b950eb72924a0475cbe8ae05fbe
SebastienRigaud <sebastien.rigaud42@gmail.com>
2018-05-31 10:22:27 +0200

    Frame du choix du mode

---
+++ src/view/ChooserFrame.java
@@ -0,0 +1,56 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChooserFrame extends JFrame implements ActionListener {
+	
+	public ChooserFrame(){
+		super("Choix du mode");
+		createPanel();
+	}
+
+	private void createPanel() {
+		getContentPane().setLayout(new BorderLayout(10,10));
+		
+		JPanel buttonPanel = new JPanel();
+		JButton controlledMode = new JButton("Tortues controlées");
+		controlledMode.setActionCommand("controlledMode");
+		controlledMode.addActionListener(this);
+		JButton automaticMode = new JButton("Tortues automatiques");
+		controlledMode.setActionCommand("automaticMode");
+		controlledMode.addActionListener(this);
+		
+		buttonPanel.add(controlledMode);
+		buttonPanel.add(automaticMode);
+		
+		getContentPane().add(buttonPanel, "Center");
+		
+		pack();
+		setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String c = e.getActionCommand();		
+		
+		SimpleLogo mainFrame = null;
+		if(c.equals("controlledMode")){
+			mainFrame = new SimpleLogo();
+		} else if (c.equals("automaticMode")){
+			mainFrame = new SimpleLogo();
+		}
+		
+		mainFrame.setVisible(true);
+		this.dispose();
+	}
+	
+	
+
+}
